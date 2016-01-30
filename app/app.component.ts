5912fbe7b97e56ce169a561f08abaa4b5f6f8036
root <ggil@gctech.com.ve>
2016-01-30 06:19:39 -0430

    Fighting again, new tools, new language...

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+// Access to X Function importing the primary angular library
+import {Component} from 'angular2/core';
+
+// Apply X function Invoking it
+@Component({
+    selector: 'my-app', /* Specify CSS for host html*/
+    template: '<h1>My Third Angular 2 App</h1>' /* Html Forms for render a view */
+})
+
+// Export file as a Component
+export class AppComponent { }
---
+++ app/boot.ts
@@ -0,0 +1,7 @@
+// Angular Browser
+import {bootstrap}    from 'angular2/platform/browser'
+// Applications root
+import {AppComponent} from './app.component'
+
+// Call bootstrap passing both params
+bootstrap(AppComponent);
